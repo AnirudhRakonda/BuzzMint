3e97a0eeece55fa7cc11c7ddad3a3869682867e2
AnirudhRakonda <140954132+AnirudhRakonda@users.noreply.github.com>
2025-05-18 11:34:27 +0530

    error fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
